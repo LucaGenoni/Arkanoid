ee8e2d2786ab8c374b59be896c76f154ad064768
LucaGenoni <lucageno9619@gmail.com>
2021-05-30 12:31:15 +0200

    display blocks
    test:
     - to load a level from a map which describe the position of blocks.
     - to display the block of the level on the canvas

--- gameModel.js
+++ gameModel.js
@@ -1,3 +1,25 @@
+vs = `#version 300 es
+
+in vec4 a_position;
+
+uniform mat4 u_matrix;
+
+void main() {
+  // Multiply the position by the matrix M_{WVP}.
+  gl_Position = u_matrix * a_position;
+}
+`
+fs = `#version 300 es
+precision highp float;
+
+uniform vec4 u_color;
+
+out vec4 outColor;
+
+void main() {
+   outColor = u_color;
+}
+`;
 class Arkanoid {
 	constructor(params) {
 		console.log("Preparing the game...")
@@ -9,8 +31,64 @@ class Arkanoid {
 		this.one = true
 		document.onkeydown = (keyDownEvent) => this.keyDown(keyDownEvent)
 		// window.addEventListener("keydown", this.keyDown, false);
-	}
+		// 
+		this.geometryBlock = {	//block
+			position: [1,1,-1,1,1,1,1,-1,1,1,-1,-1,-1,1,1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,1,1,1,1,1,1,1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,1,-1,1,-1,-1,1,1,1,1,-1,1,1,-1,-1,1,1,-1,1,-1,1,-1,1,1,-1,1,-1,-1,-1,-1,-1],
+			// normal: [1,0,0,1,0,0,1,0,0,1,0,0,-1,0,0,-1,0,0,-1,0,0,-1,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,-1,0,0,-1,0,0,-1,0,0,-1,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,-1,0,0,-1,0,0,-1,0,0,-1],
+			// texcoord: [1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1],
+			indices: [0,1,2,0,2,3,4,5,6,4,6,7,8,9,10,8,10,11,12,13,14,12,14,15,16,17,18,16,18,19,20,21,22,20,22,23],
+		};
+		this.programInfo = twgl.createProgramInfo(gl, [vs, fs]);
+		twgl.setAttributePrefix("a_");
+		var colors = [[1, 0.250980392, 0, 1],[1, 0.501960784, 0, 1],[1, 0.749019608, 0, 1],[1, 1, 0, 1],[0.749019608, 1, 0, 1],[0.501960784, 1, 0, 1],[0.250980392, 1, 0, 1],[0, 1, 0, 1],[0, 1, 0.250980392, 1],[0, 1, 0.501960784, 1],[0, 1, 0.749019608, 1],[0, 1, 1, 1],[0, 0.749019608, 1, 1],[0, 0.501960784, 1, 1],[0, 0.250980392, 1, 1],[0, 0, 1, 1],[0.250980392, 0, 1, 1],[0.501960784, 0, 1, 1],[0.749019608, 0, 1, 1],[1, 0, 1, 1],[1, 0, 0.749019608, 1],[1, 0, 0.501960784, 1],[1, 0, 0.250980392, 1],[1, 0, 0, 1],];
+		
+		this.block = []
+		this.ball = []
+		// loading meshes blocks
+		for (x=0;x<map.length;x++){
+			for (y=0;y<map[x].length;y++){
+				var typeBlock = map[x][y]
+				if (typeBlock!=0){					
+					var signleColor = Math.floor(Math.random() * colors.length);
+					this.block.push({
+						//uniforms like color, textures and other things
+						
+						uniforms: { 
+							u_color: colors[signleColor],
+							// u_matrix: utils.MakeTranslateMatrix((2*x-map.length)/map.length,(2*y-map[x].length)/map[x].length,0)
+							u_matrix: utils.transposeMatrix(utils.multiplyMatrices(
+								utils.MakeTranslateMatrix((2*x-map.length)/map.length,(2*y-map[x].length)/map[x].length,0),
+								utils.MakeScaleMatrix(0.1)
+							))
+						},	
+						//shaders
+						programInfo: this.programInfo,
+						//geometry
+						bufferInfo: twgl.createBufferInfoFromArrays(gl, this.geometryBlock),
+					})
+				}else{
+					this.block.push({
+						//uniforms like color, textures and other things
+						uniforms: { 
+							u_color: [0,0,0,1],
+							// u_matrix: utils.MakeTranslateMatrix((2*x-map.length)/map.length,(2*y-map[x].length)/map[x].length,0)
+							u_matrix: utils.transposeMatrix(utils.multiplyMatrices(
+								utils.MakeTranslateMatrix((2*x-map.length)/map.length,(2*y-map[x].length)/map[x].length,0),
+								utils.MakeScaleMatrix(0.1)
+							))
+						},	
+						//shaders
+						programInfo: this.programInfo,
+						//geometry
+						bufferInfo: twgl.createBufferInfoFromArrays(gl, this.geometryBlock),
+					})
+				}
+			}
+		}
 
+	}
+	
+	
 	keyDown(e){
 		console.log(this.params,e);
 		if (e.code=="Escape") this.state = "Pause"
@@ -78,14 +156,44 @@ class Arkanoid {
 			case "Playing":
 				if (this.one) console.log(this.state)
 				if (this.one) this.one = ! this.one
-				// change keybindings
-				// fetch data
-					// check collisions
-						// modify coordinate and direction
-				requestAnimationFrame(this.drawScene);
-				
+				requestAnimationFrame(this.render);
 				break;	
 			default:
+				break;
+		}
+	}
+	
+	render(time) {		
+		//this function must work with globals   
+		switch (game.state) {
+			case "Starting":
+			case "Playing":
+				console.log(game.state,game.balPosition,game.ballAngle,game.barPosition)
+				// rendering
+				// scene.updateWorldMatrices();	
+				// var viewProj = m4.multiply(proj,view)
+				// this.nodeObjects.forEach(e => {
+				// 	e.drawInfo.uniforms.u_matrix = m4.multiply(viewProj, e.worldMatrix)
+				// 	//extention for detection
+				// 		if(e.drawDetectionID) {
+				// 			e.drawDetectionID.uniforms.u_matrix = e.drawInfo.uniforms.u_matrix
+				// 			e.drawDetectionColor.uniforms.u_matrix = e.drawInfo.uniforms.u_matrix					
+				// 		}
+				// });t
+				
+				twgl.resizeCanvasToDisplaySize(gl.canvas);
+				gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+			
+				twgl.drawObjectList(gl, game.block);
+				// gl.useProgram(game.programInfo.program);
+				// twgl.setBuffersAndAttributes(gl, game.block[0].programInfo, game.block[0].bufferInfo);
+				// twgl.setUniforms(game.block[0].programInfo, game.block[0].uniforms);
+				// twgl.drawBufferInfo(gl, game.block[0]);
+			
+				requestAnimationFrame(game.render);
+				break;                    
+			default:
+				console.log("Block refresh");
 				break;
 		}
 	}
@@ -95,6 +203,18 @@ class Arkanoid {
 			case "Starting":
 			case "Playing":
 				console.log(game.state,game.balPosition,game.ballAngle,game.barPosition)
+				// rendering
+				// scene.updateWorldMatrices();	
+				// var viewProj = m4.multiply(proj,view)
+				// this.nodeObjects.forEach(e => {
+				// 	e.drawInfo.uniforms.u_matrix = m4.multiply(viewProj, e.worldMatrix)
+				// 	//extention for detection
+				// 		if(e.drawDetectionID) {
+				// 			e.drawDetectionID.uniforms.u_matrix = e.drawInfo.uniforms.u_matrix
+				// 			e.drawDetectionColor.uniforms.u_matrix = e.drawInfo.uniforms.u_matrix					
+				// 		}
+				// });
+				twgl.drawObjectList(gl, game.block);
 				requestAnimationFrame(game.drawScene);
 				break;                    
 			default:
@@ -103,5 +223,3 @@ class Arkanoid {
 		}
 	}
 }
-const game = new Arkanoid(1);
-game.play()
