3c5f37a7a002d7c8dd33bb4ba72d7618bedf4d14
rpigozzi <riccardo.pigozzi@mail.polimi.it>
2021-06-20 10:34:55 +0200

    Added first parts of powerups

--- gameModel.js
+++ gameModel.js
@@ -22,18 +22,23 @@ void main() {
 `;
 class Arkanoid {
 	constructor(params) {
-		var ballPosition = [0.0,-0.8];
-		var barPosition = [0,-1];
 		console.log("Preparing the game...");
 		this.params = params;
 		this.state = "Starting";
+		this.isGameStopped = true;
+		
 		this.ballAngle = 90;
+
+		var ballPosition = [0.0,-0.8];
+		var barPosition = [0,-1];
+		
 		this.score = 0;
 		this.lives = 3;
+		this.pity = 0;
+		
 		this.velocity = 0.02;
 		this.velocityBar = 0.02;
-		this.isGameStopped = true;
-		// window.addEventListener("keydown", this.keyDown, false);
+		
 		// document.onkeydown = (keyDownEvent) => this.keyDown(keyDownEvent);
 		// window.addEventListener("keydown", this.keyDown, false);
 		// 
@@ -88,42 +93,81 @@ class Arkanoid {
 		}
 
 		this.block = [];
+		this.powerUps = [];
 		// loading meshes blocks
-		for (x=0;x<map.length;x++){
-			for (y=0;y<map[x].length;y++){
+		for (var x = 0; x < map.length; x++){
+			for (var y = 0; y < map[x].length; y++){
 				var typeBlock = map[x][y];
 				if (typeBlock != 0){ //to add different typeblock make more cases with 1,2,3 block
 					var signleColor = Math.floor(Math.random() * colors.length);
 					var coordinate = [(2*x-map.length+1)/map.length,(2*y-map[x].length+1)/map[x].length,0];
 					var scaling = 0.1;
-					this.block.push({
-						//uniforms like color, textures and other things						
-						center:coordinate,
-						dimensions: [scaling,scaling,scaling],
-						spigoli:[],
-						localMatrix : utils.multiplyMatrices(
-							utils.MakeTranslateMatrix(coordinate[0],coordinate[1],0),
-							utils.MakeScaleMatrix(scaling)
-						),
-						uniforms: {
-							u_color: colors[signleColor],
-							// u_matrix: utils.MakeTranslateMatrix((2*x-map.length)/map.length,(2*y-map[x].length)/map[x].length,0)
-							u_matrix: utils.transposeMatrix(utils.multiplyMatrices(
+
+					// add to the list a block hiding a powerup, and also add the powerup to its list
+					// (powerup hidden behind the block only if randomly extracted by willHavePowerUp, and if the number
+					// of upgrades already hidden doesn't overcome the limit)
+					if (this.willHavePowerUp() && this.powerUps.length <= Math.floor(this.block.length / 5)){
+
+						this.powerUps.push(new powerUp(this, this.powerUps.length + 1));
+						
+						this.block.push({
+							hasPowerUp: true,
+							id: this.powerUps.length,
+							//uniforms like color, textures and other things						
+							center:coordinate,
+							dimensions: [scaling,scaling,scaling],
+							spigoli:[],
+							localMatrix : utils.multiplyMatrices(
 								utils.MakeTranslateMatrix(coordinate[0],coordinate[1],0),
 								utils.MakeScaleMatrix(scaling)
-							))
-						},
-						//shaders
-						programInfo: this.programInfo,
-						//geometry
-						bufferInfo: twgl.createBufferInfoFromArrays(gl, this.geometryBlock),
-					});
+							),
+							uniforms: {
+								u_color: colors[signleColor],
+								// u_matrix: utils.MakeTranslateMatrix((2*x-map.length)/map.length,(2*y-map[x].length)/map[x].length,0)
+								u_matrix: utils.transposeMatrix(utils.multiplyMatrices(
+									utils.MakeTranslateMatrix(coordinate[0],coordinate[1],0),
+									utils.MakeScaleMatrix(scaling)
+								))
+							},
+							//shaders
+							programInfo: this.programInfo,
+							//geometry
+							bufferInfo: twgl.createBufferInfoFromArrays(gl, this.geometryBlock),
+						});
+					}
+					
+					//add to the list a block without a powerup
+					else {
+						this.block.push({
+							hasPowerUp: false,
+							//uniforms like color, textures and other things						
+							center:coordinate,
+							dimensions: [scaling,scaling,scaling],
+							spigoli:[],
+							localMatrix : utils.multiplyMatrices(
+								utils.MakeTranslateMatrix(coordinate[0],coordinate[1],0),
+								utils.MakeScaleMatrix(scaling)
+							),
+							uniforms: {
+								u_color: colors[signleColor],
+								// u_matrix: utils.MakeTranslateMatrix((2*x-map.length)/map.length,(2*y-map[x].length)/map[x].length,0)
+								u_matrix: utils.transposeMatrix(utils.multiplyMatrices(
+									utils.MakeTranslateMatrix(coordinate[0],coordinate[1],0),
+									utils.MakeScaleMatrix(scaling)
+								))
+							},
+							//shaders
+							programInfo: this.programInfo,
+							//geometry
+							bufferInfo: twgl.createBufferInfoFromArrays(gl, this.geometryBlock),
+						});
+					}
+					
+					
 					this.updateSpigoliObject(this.block[x]);
 				}
 			}
 		}
-
-
 	}
 	
 	
@@ -240,7 +284,7 @@ class Arkanoid {
 				if(Math.abs(game.ball.center[0])>1-game.ball.radius) game.ball.direction[0] = -game.ball.direction[0];
 				if(game.ball.center[1]>1-game.ball.radius) game.ball.direction[1] = -game.ball.direction[1];
 				if(game.ball.center[1]<-1+game.ball.radius) {
-					console.log("lost a life or the game")
+					console.log("lost a life or the game");
 					game.state = "Pause";
 					game.handleLifeLoss();
 					break;
@@ -329,16 +373,17 @@ class Arkanoid {
 			VP,utils.multiplyMatrices(
 			utils.MakeTranslateMatrix(game.ball.center[0],game.ball.center[1],0),
 			utils.MakeScaleNuMatrix(0.1,0.1,0.1)
-		)))
+		)));
 		
 		game.bar.uniforms.u_matrix = utils.transposeMatrix(utils.multiplyMatrices(
 			VP,utils.multiplyMatrices(
 			utils.MakeTranslateMatrix(game.bar.center[0],game.bar.center[1],0),
 			utils.MakeScaleNuMatrix(0.2,0.1,0.1)
-		)))
+		)));
 		game.block.forEach(e => {
 			e.uniforms.u_matrix = utils.transposeMatrix(utils.multiplyMatrices(VP,e.localMatrix))
 		});
+		
 		twgl.resizeCanvasToDisplaySize(gl.canvas);
 		gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 	
@@ -346,6 +391,29 @@ class Arkanoid {
 		game.drawSingleObject(game.ball);
 		twgl.drawObjectList(gl, game.block);
 		requestAnimationFrame(game.render);
+	}
+	
+	drawSingleObject(item){		
+		gl.useProgram(item.programInfo.program);
+		twgl.setBuffersAndAttributes(gl, item.programInfo, item.bufferInfo);
+		twgl.setUniforms(item.programInfo, item.uniforms);
+		twgl.drawBufferInfo(gl, item.bufferInfo);
+	}
+	
+	willHavePowerUp(){
+		var prob = Math.random();
+
+		// randomically decides to add a powerup or not to the block: subsequent negative cases result in an increase 
+		// of the pity, which increases the probability that the next block will have a powerup
+		if (Math.random() + this.pity > 0.6){
+			this.pity = 0;
+			return true;
+		}
+		
+		else {
+			this.pity += 0.1;
+			return false;
+		}
 	}
 
 	/* <----------------- Functions for UI -----------------> */
---
+++ powerUp.js
@@ -0,0 +1,11 @@
+﻿class powerUp {
+    constructor(game, id){
+        this.arkanoidGame = game;
+        this.id = id;
+        this.type = Math.floor(4* Math.random()) + 1; //identifies the upgrade (possible values: 1, 2, 3, 4)
+    }
+
+    getType(){
+        return this.type;
+    }
+}
